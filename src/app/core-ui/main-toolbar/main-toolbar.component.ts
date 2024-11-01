19e222838c0926484de78b7e915a569091f6229b
Dusty Okra <dustyokra@gmail.com>
2024-10-31 17:40:33 -0700

    Theme stuff. Revert some of this. (Particularly pink-wave-theme.)

--- src/app/core-ui/main-toolbar/main-toolbar.component.ts
+++ src/app/core-ui/main-toolbar/main-toolbar.component.ts
@@ -1,6 +1,6 @@
 import { AsyncPipe } from '@angular/common';
 import { ChangeDetectionStrategy, Component, inject, input } from '@angular/core';
-import { MatIconButton } from '@angular/material/button';
+import { MatButton, MatIconButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
 import { MatSidenav } from '@angular/material/sidenav';
 import { MatSlideToggle, MatSlideToggleChange } from '@angular/material/slide-toggle';
@@ -8,12 +8,15 @@ import { MatToolbar } from '@angular/material/toolbar';
 import { Observable } from 'rxjs';
 import { DeviceTypeService } from '../../core/device-type/device-type.service';
 
+export type ThemeType = 'default-theme' | 'wave-theme' | 'pink-wave-theme';
+
 @Component({
   selector: 'app-main-toolbar',
   standalone: true,
   imports: [
     AsyncPipe,
     MatIcon,
+    MatButton,
     MatIconButton,
     MatSlideToggle,
     MatToolbar,
@@ -26,7 +29,8 @@ export class MainToolbarComponent {
   public drawer = input<MatSidenav>();
   public readonly isHandset$: Observable<boolean>;
   private readonly deviceTypeService: DeviceTypeService = inject(DeviceTypeService);
-  public theme: 'light' | 'dark' = 'light';
+  // public theme: 'light' | 'dark' = 'light';
+  public theme: ThemeType = 'default-theme';
 
   constructor() {
     this.isHandset$ = this.deviceTypeService.isHandset$;
@@ -40,12 +44,28 @@ export class MainToolbarComponent {
     }
 
     if (event.checked) {
-      this.theme = 'dark';
+      this.theme = 'wave-theme';
     } else {
-      this.theme = 'light';
+      this.theme = 'default-theme';
     }
 
-    body.classList.toggle('dark');
-    body.classList.toggle('light');
+    body.classList.toggle('wave-theme');
+    body.classList.toggle('default-theme');
+  }
+
+  public switchTheme2(themeName: ThemeType): void {
+    const body: HTMLBodyElement | null = document.querySelector('body');
+
+    if (!body) {
+      return;
+    }
+
+    this.theme = themeName;
+
+    body.classList.remove('pink-wave-theme');
+    body.classList.remove('wave-theme');
+    body.classList.remove('default-theme');
+
+    body.classList.add(this.theme);
   }
 }
