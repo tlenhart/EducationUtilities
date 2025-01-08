1ec28f7a65fde21f2dd3c99b4c26e7016ca5b4ce
Dusty Okra <dustyokra@gmail.com>
2025-01-08 14:31:07 -0800

    Include Angular service worker hash information in app version information.

--- src/app/models/app-version.model.ts
+++ src/app/models/app-version.model.ts
@@ -1,5 +1,20 @@
+import { UnrecoverableStateEvent, VersionEvent } from '@angular/service-worker';
+
 export interface AppVersion {
   packageJsonVersion: string;
   gitVersion: string;
   buildDate: string;
+}
+
+export interface AngularHashVersion {
+  currentAngularHash: string;
+  latestAngularHash: string;
+  swEventState: 'unloaded' | VersionEvent['type'] | UnrecoverableStateEvent['type'];
+  swIsEnabled: boolean;
+  errorMessage?: string;
+}
+
+export interface FullVersionInfo {
+  appVersion: AppVersion;
+  angularVersion: AngularHashVersion;
 }
